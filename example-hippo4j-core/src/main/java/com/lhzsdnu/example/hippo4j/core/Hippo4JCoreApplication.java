465ebbfba2f5da67929314aac58ed91eab29b5df
lhzsdnu <2383376138@qq.com>
2023-04-17 21:18:48 +0800

    升级 1.4.3

--- example-hippo4j-core/src/main/java/com/lhzsdnu/example/hippo4j/core/Hippo4JCoreApplication.java
+++ example-hippo4j-core/src/main/java/com/lhzsdnu/example/hippo4j/core/Hippo4JCoreApplication.java
@@ -1,7 +1,5 @@
 package com.lhzsdnu.example.hippo4j.core;
 
-import cn.hippo4j.core.enable.EnableDynamicThreadPool;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
